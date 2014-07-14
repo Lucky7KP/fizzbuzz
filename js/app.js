51a2add73c5df807f561d1690d2999803b3dfb8e
Lucky7KP <kpeltier@Kristinas-MacBook-Pro.local>
2014-07-13 22:23:27 -0700

    corrected content in developer tools

--- js/app.js
+++ js/app.js
@@ -1,41 +1,16 @@
-$document.ready(function(){
+$(document).ready(function(){
     for(var counter = 1; counter <=100; counter++){
-    	if (counter % 3 === 0 && i % 5 === 0) {
+    	if (counter % 3 === 0 && counter % 5 === 0) {
 			$("#main").append("<p>FizzBuzz</p>");
 		} 
-		else if (i % 3 === 0) {
+		else if (counter % 3 === 0) {
 			$("#main").append("<p>Fizz</p>");
 		} 
-		else if (i % 5 === 0) {
+		else if (counter % 5 === 0) {
 			$("#main").append("<p>Buzz</p>");
 		}
 		else {
 	    	$("#main").append(counter);
 		}
 }
-});
-
-$(document).ready(function () {
-   for (var counter=1;counter<=100;counter++) {
-        if (counter % 3 === 0 && i % 5 === 0) {
-                $("#main").append("<p>FizzBuzz</p>");
-                //console.log("fizzbuzz");//
-            }
-            else {
-               $("#main").append("<p>Fizz</p>");
-               //console.log("fizz");//
-            }
-        }
-        else {
-            if(counter%5===0) {
-            $("#main").append("<p>Buzz</p>");
-            //console.log("buzz");//
-            }
-            else {
-                var result = "<p>" + counter + "</p>";
-                $("#main").append(result);
-                //console.log(counter);//
-            }
-        }
-    }
 });
