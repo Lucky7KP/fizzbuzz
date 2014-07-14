1b9ac2d5840a448cd76ccaac1a4cb3c6b906fc87
Lucky7KP <kpeltier@Kristinas-MacBook-Pro.local>
2014-07-13 22:15:08 -0700

    update content

--- js/app.js
+++ js/app.js
@@ -1,6 +1,41 @@
 $document.ready(function(){
-	$('#start').click (function() {
-		fizzbuzz();
-	});
+    for(var counter = 1; counter <=100; counter++){
+    	if (counter % 3 === 0 && i % 5 === 0) {
+			$("#main").append("<p>FizzBuzz</p>");
+		} 
+		else if (i % 3 === 0) {
+			$("#main").append("<p>Fizz</p>");
+		} 
+		else if (i % 5 === 0) {
+			$("#main").append("<p>Buzz</p>");
+		}
+		else {
+	    	$("#main").append(counter);
+		}
+}
+});
 
-})
+$(document).ready(function () {
+   for (var counter=1;counter<=100;counter++) {
+        if (counter % 3 === 0 && i % 5 === 0) {
+                $("#main").append("<p>FizzBuzz</p>");
+                //console.log("fizzbuzz");//
+            }
+            else {
+               $("#main").append("<p>Fizz</p>");
+               //console.log("fizz");//
+            }
+        }
+        else {
+            if(counter%5===0) {
+            $("#main").append("<p>Buzz</p>");
+            //console.log("buzz");//
+            }
+            else {
+                var result = "<p>" + counter + "</p>";
+                $("#main").append(result);
+                //console.log(counter);//
+            }
+        }
+    }
+});
